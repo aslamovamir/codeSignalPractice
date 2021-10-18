25e610e76470a73d8142bc51ca85c9b88e24f7ba
Amir Aslamov <90361791+aslamovamir@users.noreply.github.com>
2021-10-18 16:48:08 -0400

    Create avoidObstacles.cpp

---
+++ avoidObstacles.cpp
@@ -0,0 +1,40 @@
+// You are given an array of integers representing coordinates of obstacles situated on a straight line.
+
+// Assume that you are jumping from the point with coordinate 0 to the right. You are allowed only to make jumps of the same length represented by some integer.
+
+// Find the minimal length of the jump enough to avoid all the obstacles.
+
+#include <algorithm>
+
+int avoidObstacles(vector<int> inputArray) {
+    
+    //first we sort the array
+    sort(inputArray.begin(), inputArray.end());
+    //we need to jump thrugh the entire array, therefore
+    //we need to determine the lenght of thr path
+    int largest_val = inputArray[0];
+    for (int i = 0; i < inputArray.size(); i++) {
+        if (largest_val < inputArray[i]) {
+            largest_val = inputArray[i];
+        }
+    }
+    
+    bool collided = false;
+    int curr_point = 0;
+    int min_jumps = 1; //smallest default
+    for (int i = 0; i <= largest_val; i++) {
+        collided = false;
+        curr_point += min_jumps;
+        for (int j = 0; j < inputArray.size(); j++) {
+            if (curr_point == inputArray[j]) {
+                collided = true;
+                break;
+            }
+        }
+        if (collided) {
+            min_jumps++;
+            curr_point = 0;
+            i = -1;
+        }
+    }
+    
