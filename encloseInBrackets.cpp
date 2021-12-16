6a8ebe8a0ab10fb69e58ed948d2409373d4237bd
Amir Aslamov <90361791+aslamovamir@users.noreply.github.com>
2021-12-16 15:44:28 -0500

    Create encloseInBrackets.cpp

---
+++ encloseInBrackets.cpp
@@ -0,0 +1,10 @@
+// Given a string, enclose it in round brackets.
+
+// Example
+
+// For inputString = "abacaba", the output should be
+// solution(inputString) = "(abacaba)".
+
+std::string solution(std::string inputString) {
+    return "(" + inputString + ")";
+}
